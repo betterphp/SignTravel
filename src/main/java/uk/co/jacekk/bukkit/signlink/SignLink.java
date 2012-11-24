4dc8b4853fa313f655daeeebda00cb1d2a1768a4
Jacek <jacek@kuzemczak.co.uk>
2012-11-24 21:33:21 +0000

    Updated for BasePlugin 5.0

---
+++ src/main/java/uk/co/jacekk/bukkit/signlink/SignLink.java
@@ -0,0 +1,36 @@
+package uk.co.jacekk.bukkit.signlink;
+
+import java.io.File;
+
+import uk.co.jacekk.bukkit.baseplugin.v5.BasePlugin;
+import uk.co.jacekk.bukkit.signlink.listeners.SignBreakListener;
+import uk.co.jacekk.bukkit.signlink.listeners.SignCreateListener;
+import uk.co.jacekk.bukkit.signlink.listeners.TeleportListener;
+import uk.co.jacekk.bukkit.util.LocationStore;
+
+public class SignLink extends BasePlugin {
+	
+	public LocationStore locations;
+	
+	public void onEnable(){
+		super.onEnable(true);
+		
+		File locationFile = new File(this.baseDirPath + File.separator + "sign-locations.bin");
+		
+		this.locations = new LocationStore(locationFile);
+		this.locations.load();
+		
+		if (locationFile.canWrite() == false){
+			this.log.fatal("Unable to write to the location storage file.");
+		}
+		
+		this.pluginManager.registerEvents(new TeleportListener(this), this);
+		this.pluginManager.registerEvents(new SignCreateListener(this), this);
+		this.pluginManager.registerEvents(new SignBreakListener(this), this);
+	}
+	
+	public void onDisable(){
+		this.locations.save();
+	}
+	
+}
---
+++ src/main/java/uk/co/jacekk/bukkit/signlink/listeners/SignBreakListener.java
@@ -0,0 +1,95 @@
+package uk.co.jacekk.bukkit.signlink.listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import uk.co.jacekk.bukkit.baseplugin.v5.event.BaseListener;
+import uk.co.jacekk.bukkit.signlink.SignLink;
+
+public class SignBreakListener extends BaseListener<SignLink> {
+	
+	public SignBreakListener(SignLink plugin){
+		super(plugin);
+	}
+	
+	private boolean checkSignBreak(Block block, Player player){
+		Sign sign = (Sign) block.getState();
+		
+		String[] lines = sign.getLines();
+		
+		String line;
+		String location = null;
+		String destination = null;
+		
+		for (int i = 3; i >= 0; --i){
+			line = lines[i]; 
+			
+			if (line.startsWith("[") && line.endsWith("]")){
+				if (destination == null){
+					destination = line.substring(1, line.length() - 1);
+				}else{
+					location = line.substring(1, line.length() - 1);
+				}
+			}
+		}
+		
+		if (location != null && plugin.locations.contains(location)){
+			if (player.hasPermission("signlink.remove") == false){
+				player.sendMessage(plugin.formatMessage(ChatColor.RED + "You do not have permission to remove this sign"));
+				return false;
+			}
+			
+			plugin.locations.remove(location);
+			player.sendMessage(plugin.formatMessage(ChatColor.GREEN + "Location removed"));
+		}else if (destination != null && plugin.locations.contains(destination)){
+			if (player.hasPermission("signlink.remove") == false){
+				player.sendMessage(plugin.formatMessage(ChatColor.RED + "You do not have permission to remove this sign"));
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent event){
+		Player player = event.getPlayer();
+		Block broken = event.getBlock();
+		
+		Material brokenType = broken.getType();
+		
+		if ((brokenType == Material.SIGN_POST || brokenType == Material.WALL_SIGN) && this.checkSignBreak(broken, player) == false){
+			event.setCancelled(true);
+			return;
+		}
+		
+		Block above = broken.getRelative(BlockFace.UP);
+		
+		if (above.getType() == Material.SIGN_POST && this.checkSignBreak(above, player) == false){
+			event.setCancelled(true);
+			return;
+		}
+		
+		Block[] surrounding = new Block[4];
+		
+		surrounding[0] = broken.getRelative(BlockFace.NORTH);
+		surrounding[1] = broken.getRelative(BlockFace.SOUTH);
+		surrounding[2] = broken.getRelative(BlockFace.EAST);
+		surrounding[3] = broken.getRelative(BlockFace.WEST);
+		
+		for (Block surroundingBlock : surrounding){
+			if (surroundingBlock.getType() == Material.WALL_SIGN && this.checkSignBreak(surroundingBlock, player) == false){
+				event.setCancelled(true);
+				return;
+			}
+		}
+	}
+	
+}
