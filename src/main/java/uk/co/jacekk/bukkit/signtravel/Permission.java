5eb8eab551486e3ba434a904618f117f874ef065
Jacek <jacek@kuzemczak.co.uk>
2013-02-04 23:54:40 +0000

    Corrected permissions still using the old name.

--- src/main/java/uk/co/jacekk/bukkit/signtravel/Permission.java
+++ src/main/java/uk/co/jacekk/bukkit/signtravel/Permission.java
@@ -6,10 +6,10 @@ import uk.co.jacekk.bukkit.baseplugin.v8.permissions.PluginPermission;
 
 public class Permission {
 	
-	public static final PluginPermission SIGN_CREATE		= new PluginPermission("signlink.sign.create",		PermissionDefault.OP,	"Allows the player to create SignLink signs");
-	public static final PluginPermission SIGN_REMOVE		= new PluginPermission("signlink.sign.remove",		PermissionDefault.OP,	"Allows the player to remove SignLink signs");
-	public static final PluginPermission SIGN_USE			= new PluginPermission("signlink.sign.use",			PermissionDefault.TRUE,	"Allows the player to use SignLink signs");
-	public static final PluginPermission COMMAND_WARP		= new PluginPermission("signlink.command.warp",		PermissionDefault.TRUE,	"Allows the player to use the /warp command");
-	public static final PluginPermission COMMAND_WARPLIST	= new PluginPermission("signlink.command.warplist",	PermissionDefault.TRUE,	"Allows the player to use the /warplist command");
+	public static final PluginPermission SIGN_CREATE		= new PluginPermission("signtravel.sign.create",		PermissionDefault.OP,	"Allows the player to create SignTravel signs");
+	public static final PluginPermission SIGN_REMOVE		= new PluginPermission("signtravel.sign.remove",		PermissionDefault.OP,	"Allows the player to remove SignTravel signs");
+	public static final PluginPermission SIGN_USE			= new PluginPermission("signtravel.sign.use",			PermissionDefault.TRUE,	"Allows the player to use SignTravel signs");
+	public static final PluginPermission COMMAND_WARP		= new PluginPermission("signtravel.command.warp",		PermissionDefault.TRUE,	"Allows the player to use the /warp command");
+	public static final PluginPermission COMMAND_WARPLIST	= new PluginPermission("signtravel.command.warplist",	PermissionDefault.TRUE,	"Allows the player to use the /warplist command");
 	
 }
